1aa870fb153a0b7613060e384efaa81bc310e608
Siftal <m.khanmohamadzadeh@gmail.com>
2019-10-19 08:41:23 -0400

    rename contracts & remove claim function

--- contracts/SubscriptionsMinter.sol
+++ contracts/SubscriptionsMinter.sol
@@ -1,20 +1,20 @@
 pragma solidity ^0.5.0;
 
 import "/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "./BSToken.sol";
-import "./BSSToken.sol";
+import "/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "./Sponsorships.sol";
+import "./Subscriptions.sol";
 import "./Finance.sol";
 import "./CanReclaimToken.sol";
 
 
 /**
- * @title BSST minter contract.
+ * @title Subscriptions minter contract.
  */
-contract BSSTMinter is CanReclaimToken {
+contract SubscriptionsMinter is CanReclaimToken {
     using SafeMath for uint256;
 
-    BSToken internal bsToken;
-    BSSToken internal bssToken;
+    Subscriptions internal subs;
     ERC20 internal purchaseToken;
 
     uint256 public cap;
@@ -23,36 +23,34 @@ contract BSSTMinter is CanReclaimToken {
     string private constant INSUFFICIENT_PAYMENT = "Insufficient payment";
     string private constant APPROVE_ERROR = "Approve error";
     string private constant MINT_ERROR = "Mint error";
-    string private constant FINANCE_MESSAGE = "Revenue of BSS token sale";
+    string private constant FINANCE_MESSAGE = "Revenue of Subscriptions sale";
     string private constant CAP_EXCEEDED = "Cap exceeded";
 
     mapping(uint256 => uint256) private prices;
 
-    event TokensPurchased(address account, uint256 price);
-    event TokensClaimed(address account, uint256 amount);
+    event SubscriptionsPurchased(address account, uint256 price);
 
-    constructor(address bstAddr, address bsstAddr, address purchaseTokenAddr, address financeAddr)
+    constructor(address subsAddr, address purchaseTokenAddr, address financeAddr)
         public
     {
         prices[0] = 16 * 10**18;
         prices[1] = 25 * 10**18;
         prices[2] = 50 * 10**18;
         prices[3] = 100 * 10**18;
-        bsToken = BSToken(bstAddr);
-        bssToken = BSSToken(bsstAddr);
+        subs = Subscriptions(subsAddr);
         finance = Finance(financeAddr);
         purchaseToken = ERC20(purchaseTokenAddr);
-        cap = bssToken.cap();
+        cap = subs.cap();
     }
 
     /**
-     * @notice Purchase BSS token.
+     * @notice Purchase Subscriptions.
      */
     function purchase()
         external
         returns (bool success)
     {
-        uint256 totalSupply = bssToken.totalSupply();
+        uint256 totalSupply = subs.totalSupply();
         require(totalSupply < cap, CAP_EXCEEDED);
 
         uint256 stepNumber = totalSupply.div(STEP);
@@ -60,18 +58,18 @@ contract BSSTMinter is CanReclaimToken {
         uint256 allowance = purchaseToken.allowance(msg.sender, address(this));
         require(price <= allowance, INSUFFICIENT_PAYMENT);
 
-        uint256 availableTokens = (stepNumber + 1) * STEP - totalSupply;
-        uint256 bsstAmount = allowance.div(price);
-        if (availableTokens < bsstAmount) {
-            bsstAmount = availableTokens;
+        uint256 availableSubs = (stepNumber + 1) * STEP - totalSupply;
+        uint256 subsAmount = allowance.div(price);
+        if (availableSubs < subsAmount) {
+            subsAmount = availableSubs;
         }
-        uint256 purchaseTokenAmount = bsstAmount.mul(price);
+        uint256 purchaseTokenAmount = subsAmount.mul(price);
         if (purchaseToken.transferFrom(msg.sender, address(this), purchaseTokenAmount)) {
             require(purchaseToken.approve(address(finance), purchaseTokenAmount), APPROVE_ERROR);
 
             finance.deposit(address(purchaseToken), purchaseTokenAmount, FINANCE_MESSAGE);
-            emit TokensPurchased(msg.sender, bsstAmount);
-            require(bssToken.mint(msg.sender, bsstAmount), MINT_ERROR);
+            emit SubscriptionsPurchased(msg.sender, subsAmount);
+            require(subs.mint(msg.sender, subsAmount), MINT_ERROR);
 
             return true;
         }
@@ -79,27 +77,14 @@ contract BSSTMinter is CanReclaimToken {
     }
 
     /**
-     * @notice claim BST
-     */
-    function claim()
-        external
-        returns (bool success)
-    {
-        uint256 claimableAmount = bssToken.claim(msg.sender);
-        emit TokensClaimed(msg.sender, claimableAmount);
-        require(bsToken.mint(msg.sender, claimableAmount), MINT_ERROR);
-        return true;
-    }
-
-    /**
-     * @notice Get current BSS token price.
+     * @notice Get current Subscriptions price.
      */
     function price()
         external
         view
         returns (uint256)
     {
-        uint256 totalSupply = bssToken.totalSupply();
+        uint256 totalSupply = subs.totalSupply();
         if (totalSupply < cap) {
             uint256 stepNumber = totalSupply.div(STEP);
             return prices[stepNumber];
