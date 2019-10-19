122c719109e4b8663e22ab079b3ff17152323f4a
Siftal <m.khanmohamadzadeh@gmail.com>
2019-10-19 07:45:18 -0400

    add NonTransferAble.sol

---
+++ contracts/NonTransferAble.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.5.0;
+
+import "/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "/openzeppelin-solidity/contracts/access/roles/MinterRole.sol";
+
+
+contract NonTransferAble is MinterRole {
+    using SafeMath for uint256;
+
+    mapping (address => uint256) private _balances;
+
+    uint256 private _totalSupply;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a `Transfer` event with `from` set to the zero address.
+     *
+     * Requirements
+     *
+     * - `to` cannot be the zero address.
+     */
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "NonTransferAbleToken: mint to the zero address");
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+}
