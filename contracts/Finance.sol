3847b064e5eabd7a116faa7c51a32e46201530bd
Siftal <m.khanmohamadzadeh@gmail.com>
2019-10-20 06:10:58 -0400

    edit documentation

--- contracts/Finance.sol
+++ contracts/Finance.sol
@@ -5,5 +5,13 @@ pragma solidity ^0.5.0;
  * @title Finance interface.
  */
 contract Finance {
+
+    /**
+    * @notice Deposit Some token in the DAO
+    * @dev Deposit for approved ERC20 tokens or ETH
+    * @param _token Address of deposited token
+    * @param _amount Amount of tokens sent
+    * @param _reference Reason for payment
+    */
     function deposit(address _token, uint256 _amount, string calldata _reference) external payable;
 }
