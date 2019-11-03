88fce96a5dfe995b800bcf9686919c5bd4e48938
Adam Stallard <adam.stallard@gmail.com>
2019-11-02 23:25:40 -0700

    Add comments

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,30 @@
+// This script needs to be executed by BMAIN DAO's Agent App.
+
+// The executor acquires "ownership" roles over the Sponsorships,
+// Subscriptions, SponsorshipsMinter, and SubscriptionsMinter contracts.
+// Look for the "onlyOwner" modifier on contract functions.
+
+// The executor acquires a permanent ability to mint both Sponsorships
+// and Subscriptions to any address, and to claim Sponsorships from 
+// Subscriptions held by any address. Look for the "onlyMinter" modifier.
+
+// The executor acquires a permanent ability to pause mint and claim
+// functions for Subscriptions and mint and assignContext functions for
+// Sponsorships. Look for the "whenNotPaused" modifier.
+
 var Sp = artifacts.require('Sponsorships.sol');
 var SpMinter = artifacts.require('SponsorshipsMinter.sol');
 
 var Subs = artifacts.require('Subscriptions.sol');
 var SubsMinter = artifacts.require('SubscriptionsMinter.sol');
 
+// This needs to match the total number of Subscriptions in all 
+// steps defined in the SubscriptionsMinter contract.
 const cap = 900000;
+// The contract address of the Finance app of BMAIN DAO.
 const financeAddr = '';
+// The purchase token needs to be DAI or an equivalent. Several places define
+// a starting or base price of "1."
 const purchaseTokenAddr = '';
 
 module.exports = function (deployer) {
@@ -17,9 +36,11 @@ module.exports = function (deployer) {
     await deployer.deploy(Subs, cap);
     const instanceSubs = await Subs.deployed();
 
+    // The token used to purchase Sponsorships can be changed later.
     await deployer.deploy(SpMinter, instanceSp.address, purchaseTokenAddr, financeAddr);
     const instanceSpMinter = await SpMinter.deployed();
 
+    // The token used to purchase Subscriptions can't be changed, but the sale has a cap and is meant to end.
     await deployer.deploy(SubsMinter, instanceSp.address, instanceSubs.address, purchaseTokenAddr, financeAddr);
     const instanceSubsMinter = await SubsMinter.deployed();
 
